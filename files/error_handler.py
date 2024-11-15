6a9446f5a37db51173fe67c3e2c475ff8a8141c2
Bartosz Złotnik <35463507+bartzlot@users.noreply.github.com>
2024-11-15 22:14:01 +0100

    Update error_handler.py

--- files/error_handler.py
+++ files/error_handler.py
@@ -2,4 +2,4 @@ class ErrorHandler:
 
     @staticmethod
     def error_handling(method_name: str, error):
-        print(f"[FileManager.{method_name}] An unexpected error occurred: {error}")
+        print(f"[FileManager.{method_name}] An unexpected error occurred: [{error}]")
